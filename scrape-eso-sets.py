2a58bcb0060f4aec9963dabef36498f3b0841ba7
DamoFD <voshall@yahoo.com>
2024-02-12 22:26:34 -0800

    scrape set bonuses
    scrape set bonuses

--- scrape-eso-sets.py
+++ scrape-eso-sets.py
@@ -25,7 +25,33 @@ for index, row in enumerate(soup.select('tr'), start=1):
     slug = href.split('/')[-1]
 
     type = row.select_one('small').get_text(strip=True) if row.select_one('small') else "Unknown Type"
-    set = {"id": index, "name": name, "type": type, "slug": slug}
+
+    # Find all bonuses for this set
+    bonuses = {'1_item': None, '2_items': None, '3_items': None, '4_items': None, '5_items': None}
+
+    bonuses_container = row.select_one('td:last-child')
+    if bonuses_container:
+
+        for bonus in bonuses_container.find_all('strong', class_='set-bonus'):
+            bonus_text = bonus.get_text(strip=True)
+            if bonus_text:
+                item_count = bonus_text.split(' ')[0].replace('(', '').replace(')', '')
+                item_count_key = f"{item_count}_item" if item_count == "1" else f"{item_count}_items"
+
+                full_description=""
+
+                current_element = bonus
+
+                while True:
+                    current_element = current_element.next_element
+                    if not current_element or current_element.name == 'strong':
+                        break
+                    if isinstance(current_element, str):
+                        full_description += current_element.strip() + " "
+
+                bonuses[item_count_key] = full_description.strip()
+
+    set = {"id": index, "name": name, "type": type, "slug": slug, **bonuses}
     sets.append(set)
 
 sets_json = json.dumps(sets, indent=4)
